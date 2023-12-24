043f2f02c66d3962a0fa76d53ebcce61ccf5c093
imvenx <imvenx@proton.me>
2023-12-24 15:20:44 +0100

    add sparkle effect to mouse

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,42 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  document.addEventListener('mousemove', createSparklesAroundMouse);
+})
+
+function createSparklesAroundMouse(e: MouseEvent) {
+  const sparkle = document.createElement('div');
+  sparkle.classList.add('sparkle');
+
+  const size = Math.random() * 3 + 2;
+  sparkle.style.width = `${size}px`;
+  sparkle.style.height = `${size}px`;
+
+  sparkle.style.left = `${e.pageX + (Math.random() - 0.5) * 10}px`;
+  sparkle.style.top = `${e.pageY + (Math.random() - 0.5) * 10}px`;
+  document.body.appendChild(sparkle);
+
+  setTimeout(() => sparkle.remove(), 1000);
+}
 </script>
+
+<style>
+.sparkle {
+  position: absolute;
+  border-radius: 50%;
+  box-shadow: 0 0 20px 2px #fff;
+  pointer-events: none;
+  animation: fadeOut 1s forwards;
+  border: 1px solid white;
+  opacity: 1;
+}
+
+@keyframes fadeOut {
+  to {
+    opacity: 0;
+  }
+}
+</style>
